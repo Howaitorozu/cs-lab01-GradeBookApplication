abdb9c55f4c4aafcc44be54b3a81a3f389e47a10
Howaitorozu <patipyrek@gmail.com>
2020-10-03 11:23:32 +0200

    Created override for CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,29 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var studentsTwentyPercent = Students.Count * 0.2;
+            int studentsWithHigherGradeCount = 0;
+            foreach(var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    studentsWithHigherGradeCount += 1;
+                }
+            }
+            return 'A';
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            } else
+            base.CalculateStatistics();
+        }
     }
 }
